878fed9b68500cb97cc02745ce9173984ac89b15
Afjalur Rahman Rana <rana01645@gmail.com>
2023-05-10 01:09:57 +0600

    wip

---
+++ src/app/server-filters/server-filters.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-filters',
+  templateUrl: './server-filters.component.html',
+  styleUrls: ['./server-filters.component.scss']
+})
+export class ServerFiltersComponent {
+
+}
